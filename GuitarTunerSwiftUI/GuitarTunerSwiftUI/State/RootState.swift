11c3f7a8fc58a31ca2cc38362a966616a69c75b2
Kody Deda <kodydeda4@gmail.com>
2021-01-20 15:44:00 -0500

    saved guitar as a png

--- GuitarTunerSwiftUI/GuitarTunerSwiftUI/State/RootState.swift
+++ GuitarTunerSwiftUI/GuitarTunerSwiftUI/State/RootState.swift
@@ -79,32 +79,54 @@ extension Guitar {
 
 // MARK:- GuitarView
 
+struct TunerButtonsView: View {
+    let store: Store<Guitar.State, Guitar.Action>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                ForEach(viewStore.tuningNotes.map(\.rawValue).reversed(), id: \.self) { midiNote in
+                    Button(Pitch(midiNote: midiNote).description) {}
+                        .padding()
+                        .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .buttonStyle(PlainButtonStyle())
+                        .pressAction {
+                            viewStore.send(.playMidiNote(midiNote))
+                        } onRelease: {
+                            viewStore.send(.stopMidiNote(midiNote))
+                        }
+                }
+            }
+        }
+    }
+}
+
+struct TunerButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TunerButtonsView(store: Guitar.defaultStore)
+    }
+}
+
+
 struct GuitarView: View {
     let store: Store<Guitar.State, Guitar.Action>
     
     var body: some View {
         WithViewStore(store) { viewStore in
-            ZStack {
-                Image("fender_placeholder")
+            
+            HStack {
+                TunerButtonsView(store: store)
+                    .padding()
+                    .border(Color.gray)
+                Image("GuitarHeadstock")
                     .resizable()
-                    .scaledToFill()
+                    .scaledToFit()
+                    .frame(width: 2435/10, height: 6431/10)
 
-                VStack {
-                    ForEach(viewStore.tuningNotes.map(\.rawValue).reversed(), id: \.self) { midiNote in
-                        Button(Pitch(midiNote: midiNote).description) {}
-                            .padding()
-                            .background(Color.accentColor)
-                            .clipShape(Circle())
-                            .buttonStyle(PlainButtonStyle())
-                            .pressAction {
-                                viewStore.send(.playMidiNote(midiNote))
-                            } onRelease: {
-                                viewStore.send(.stopMidiNote(midiNote))
-                            }
-                    }
-                }
+                Spacer()
             }
-            .frame(width: 970/2, height: 1260/2)
+            .padding()
             .navigationTitle("Guitar Tuner")
             .toolbar {
                 ToolbarItem {
