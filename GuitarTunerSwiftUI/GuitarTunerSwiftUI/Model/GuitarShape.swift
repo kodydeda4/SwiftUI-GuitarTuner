2d983d172562ee4865f2e0f823ecc44c807b7bd3
Kody Deda <kodydeda4@gmail.com>
2021-01-25 13:28:24 -0500

    fixed padding issue

--- GuitarTunerSwiftUI/GuitarTunerSwiftUI/Model/GuitarShape.swift
+++ GuitarTunerSwiftUI/GuitarTunerSwiftUI/Model/GuitarShape.swift
@@ -9,6 +9,8 @@ import Cocoa
 import SwiftUI
 
 enum GuitarShape: String, CaseIterable {
+    case tunerLines = "TunerLines"
+    case tuningCircles = "Tuning Circles"
     case fretboard = "Fretboard"
     case fret = "Fret"
     case nut = "Nut"
@@ -22,7 +24,6 @@ enum GuitarShape: String, CaseIterable {
     case stringTreeBase = "StringTreeBase"
     case stringTreeBolt = "StringTreeBolt"
     case frontTunersPeg = "FrontTunersPeg"
-    case tunerLines = "TunerLines"
 }
 
 extension GuitarShape {
@@ -48,8 +49,8 @@ extension GuitarShape {
             shape.close()
             shape.move(to: CGPoint(x: 610.51, y: 1374))
             shape.line(to: CGPoint(x: 341.23, y: 1374))
-            shape.line(to: CGPoint(x: 341.23, y: 1539.47))
-            shape.line(to: CGPoint(x: 610.51, y: 1539.47))
+            shape.line(to: CGPoint(x: 341.23, y: 1539.48))
+            shape.line(to: CGPoint(x: 610.51, y: 1539.48))
             shape.line(to: CGPoint(x: 610.51, y: 1374))
             shape.close()
             shape.move(to: CGPoint(x: 610.51, y: 1069.28))
@@ -126,8 +127,6 @@ extension GuitarShape {
             shape.line(to: CGPoint(x: 533.53, y: 91.86))
             shape.line(to: CGPoint(x: 505.67, y: 82.12))
             shape.close()
-
-
             return shape
         }
         case .rearTunersStem: do {
@@ -238,8 +237,8 @@ extension GuitarShape {
             shape.curve(to: CGPoint(x: 469.78, y: 44.89), controlPoint1: CGPoint(x: 439.43, y: 60.37), controlPoint2: CGPoint(x: 457.42, y: 40.82))
             shape.line(to: CGPoint(x: 469.78, y: 44.87))
             shape.close()
-
-
+            
+            
             return shape
         }
         case .guitarHeadstock: do {
@@ -358,8 +357,8 @@ extension GuitarShape {
             shape.line(to: CGPoint(x: 378.77, y: 859.07))
             shape.line(to: CGPoint(x: 365, y: 835.22))
             shape.close()
-
-
+            
+            
             shape.close()
             return shape
         }
@@ -516,11 +515,11 @@ extension GuitarShape {
             shape.line(to: CGPoint(x: 314.8, y: 503.94))
             shape.line(to: CGPoint(x: 314.78, y: 497.72))
             shape.close()
-            shape.move(to: CGPoint(x: 277.71, y: 637.32))
-            shape.line(to: CGPoint(x: 119, y: 637.32))
+            shape.move(to: CGPoint(x: 277.69, y: 637.33))
+            shape.line(to: CGPoint(x: 119, y: 637.33))
             shape.line(to: CGPoint(x: 119, y: 643.54))
             shape.line(to: CGPoint(x: 277.69, y: 643.54))
-            shape.line(to: CGPoint(x: 277.71, y: 637.32))
+            shape.line(to: CGPoint(x: 277.69, y: 637.33))
             shape.close()
             shape.move(to: CGPoint(x: 225.58, y: 780))
             shape.line(to: CGPoint(x: 119, y: 780))
@@ -530,7 +529,58 @@ extension GuitarShape {
             shape.close()
             return shape
         }
-        
+        case .tuningCircles: do {
+            let shape = NSBezierPath()
+            shape.move(to: CGPoint(x: 119, y: 783.14))
+            shape.curve(to: CGPoint(x: 59.52, y: 842.62), controlPoint1: CGPoint(x: 119, y: 815.99), controlPoint2: CGPoint(x: 92.37, y: 842.62))
+            shape.curve(to: CGPoint(x: 0.04, y: 783.14), controlPoint1: CGPoint(x: 26.67, y: 842.62), controlPoint2: CGPoint(x: 0.04, y: 815.99))
+            shape.curve(to: CGPoint(x: 59.52, y: 723.66), controlPoint1: CGPoint(x: 0.04, y: 750.29), controlPoint2: CGPoint(x: 26.67, y: 723.66))
+            shape.curve(to: CGPoint(x: 101.58, y: 741.08), controlPoint1: CGPoint(x: 75.3, y: 723.66), controlPoint2: CGPoint(x: 90.42, y: 729.93))
+            shape.curve(to: CGPoint(x: 119, y: 783.14), controlPoint1: CGPoint(x: 112.73, y: 752.24), controlPoint2: CGPoint(x: 119, y: 767.36))
+            shape.line(to: CGPoint(x: 119, y: 783.14))
+            shape.close()
+            shape.move(to: CGPoint(x: 59.48, y: 584.06))
+            shape.curve(to: CGPoint(x: 0.04, y: 643.57), controlPoint1: CGPoint(x: 26.64, y: 584.08), controlPoint2: CGPoint(x: 0.03, y: 610.72))
+            shape.curve(to: CGPoint(x: 59.53, y: 703.02), controlPoint1: CGPoint(x: 0.05, y: 676.41), controlPoint2: CGPoint(x: 26.69, y: 703.03))
+            shape.curve(to: CGPoint(x: 119, y: 643.54), controlPoint1: CGPoint(x: 92.38, y: 703.01), controlPoint2: CGPoint(x: 119, y: 676.38))
+            shape.curve(to: CGPoint(x: 101.56, y: 601.47), controlPoint1: CGPoint(x: 119, y: 627.76), controlPoint2: CGPoint(x: 112.73, y: 612.62))
+            shape.curve(to: CGPoint(x: 59.48, y: 584.06), controlPoint1: CGPoint(x: 90.4, y: 590.31), controlPoint2: CGPoint(x: 75.26, y: 584.05))
+            shape.line(to: CGPoint(x: 59.48, y: 584.06))
+            shape.close()
+            shape.move(to: CGPoint(x: 59.48, y: 444.46))
+            shape.curve(to: CGPoint(x: 0.04, y: 503.97), controlPoint1: CGPoint(x: 26.64, y: 444.48), controlPoint2: CGPoint(x: 0.03, y: 471.12))
+            shape.curve(to: CGPoint(x: 59.53, y: 563.42), controlPoint1: CGPoint(x: 0.05, y: 536.81), controlPoint2: CGPoint(x: 26.69, y: 563.43))
+            shape.curve(to: CGPoint(x: 119, y: 503.94), controlPoint1: CGPoint(x: 92.38, y: 563.41), controlPoint2: CGPoint(x: 119, y: 536.78))
+            shape.curve(to: CGPoint(x: 101.56, y: 461.87), controlPoint1: CGPoint(x: 119, y: 488.16), controlPoint2: CGPoint(x: 112.73, y: 473.02))
+            shape.curve(to: CGPoint(x: 59.48, y: 444.46), controlPoint1: CGPoint(x: 90.4, y: 450.71), controlPoint2: CGPoint(x: 75.26, y: 444.45))
+            shape.line(to: CGPoint(x: 59.48, y: 444.46))
+            shape.close()
+            shape.move(to: CGPoint(x: 59.48, y: 304.86))
+            shape.curve(to: CGPoint(x: 0.04, y: 364.37), controlPoint1: CGPoint(x: 26.64, y: 304.88), controlPoint2: CGPoint(x: 0.03, y: 331.52))
+            shape.curve(to: CGPoint(x: 59.53, y: 423.82), controlPoint1: CGPoint(x: 0.05, y: 397.21), controlPoint2: CGPoint(x: 26.69, y: 423.83))
+            shape.curve(to: CGPoint(x: 119, y: 364.34), controlPoint1: CGPoint(x: 92.38, y: 423.81), controlPoint2: CGPoint(x: 119, y: 397.18))
+            shape.curve(to: CGPoint(x: 101.56, y: 322.27), controlPoint1: CGPoint(x: 119, y: 348.56), controlPoint2: CGPoint(x: 112.73, y: 333.42))
+            shape.curve(to: CGPoint(x: 59.48, y: 304.86), controlPoint1: CGPoint(x: 90.4, y: 311.11), controlPoint2: CGPoint(x: 75.26, y: 304.85))
+            shape.line(to: CGPoint(x: 59.48, y: 304.86))
+            shape.close()
+            shape.move(to: CGPoint(x: 59.48, y: 165.26))
+            shape.curve(to: CGPoint(x: 0.04, y: 224.77), controlPoint1: CGPoint(x: 26.64, y: 165.28), controlPoint2: CGPoint(x: 0.03, y: 191.92))
+            shape.curve(to: CGPoint(x: 59.53, y: 284.22), controlPoint1: CGPoint(x: 0.05, y: 257.61), controlPoint2: CGPoint(x: 26.69, y: 284.23))
+            shape.curve(to: CGPoint(x: 119, y: 224.74), controlPoint1: CGPoint(x: 92.38, y: 284.21), controlPoint2: CGPoint(x: 119, y: 257.58))
+            shape.curve(to: CGPoint(x: 101.56, y: 182.67), controlPoint1: CGPoint(x: 119, y: 208.96), controlPoint2: CGPoint(x: 112.73, y: 193.82))
+            shape.curve(to: CGPoint(x: 59.48, y: 165.26), controlPoint1: CGPoint(x: 90.4, y: 171.51), controlPoint2: CGPoint(x: 75.26, y: 165.25))
+            shape.line(to: CGPoint(x: 59.48, y: 165.26))
+            shape.close()
+            shape.move(to: CGPoint(x: 59.48, y: 25.66))
+            shape.curve(to: CGPoint(x: 0.04, y: 85.17), controlPoint1: CGPoint(x: 26.64, y: 25.68), controlPoint2: CGPoint(x: 0.03, y: 52.32))
+            shape.curve(to: CGPoint(x: 59.53, y: 144.62), controlPoint1: CGPoint(x: 0.05, y: 118.01), controlPoint2: CGPoint(x: 26.69, y: 144.63))
+            shape.curve(to: CGPoint(x: 119, y: 85.14), controlPoint1: CGPoint(x: 92.38, y: 144.61), controlPoint2: CGPoint(x: 119, y: 117.98))
+            shape.curve(to: CGPoint(x: 101.56, y: 43.07), controlPoint1: CGPoint(x: 119, y: 69.36), controlPoint2: CGPoint(x: 112.73, y: 54.22))
+            shape.curve(to: CGPoint(x: 59.48, y: 25.66), controlPoint1: CGPoint(x: 90.4, y: 31.91), controlPoint2: CGPoint(x: 75.26, y: 25.65))
+            shape.line(to: CGPoint(x: 59.48, y: 25.66))
+            shape.close()
+            return shape
+        }
         }
     }
 }
--- GuitarTunerSwiftUI/GuitarTunerSwiftUI/View/RootView.swift
+++ GuitarTunerSwiftUI/GuitarTunerSwiftUI/View/RootView.swift
@@ -1,36 +1,258 @@
 //
-//  RootView.swift
+//  GuitarView.swift
 //  GuitarTunerSwiftUI
 //
-//  Created by Kody Deda on 1/20/21.
+//  Created by Kody Deda on 1/25/21.
 //
 
+import SwiftUI
+import ComposableArchitecture
+import MusicTheory
 
-//import SwiftUI
-//import ComposableArchitecture
-//import MusicTheory
-//
-//struct RootView: View {
-//    let store: Store<Root.State, Root.Action>
-//    
-//    
-//    var body: some View {
-//        let width: CGFloat = size * GuitarShape.pathBounds.width
-//        let height: CGFloat = size * GuitarShape.pathBounds.height
-//        
-//        WithViewStore(store) { viewStore in
-//                MainView()
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//
-//
-//struct RootView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        RootView(store: Root.defaultStore)
-//    }
-//}
+struct RootView: View {
+    let store: Store<Root.State, Root.Action>
+    let notes = ["E","A","D","G","B","E"]
+    
+    @State var isFilled = false
+    let shadowRadius:CGFloat = 4
+    var primaryColor = Color.gray
+    var secondaryColor = Color.white
+    
+    @State var strokeEndAmount: CGFloat = 0
+    @State var opacity: CGFloat = 0
+    
+    let animationDuration: Double = 1
+    
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            ZStack {
+                GuitarView(isFilled: isFilled,
+                           shadowRadius: shadowRadius,
+                           primaryColor: primaryColor,
+                           secondaryColor: secondaryColor
+                )
+                .border(Color.blue)
+                TunersView(isFilled: isFilled,
+                           notes: notes,
+                           primaryColor: primaryColor
+                )
+                .border(Color.red)
+            }
+            .padding()
+            .border(Color.gray)
+            .frame(width: GuitarShape.pathBounds.width * 0.5, height: GuitarShape.pathBounds.height * 0.5)
+            
+            .navigationTitle("Guitar Tuner")
+            .onAppear {
+                withAnimation(
+                    .easeInOut(duration: animationDuration)) {
+                    strokeEndAmount = CGFloat(animationDuration)
+                }
+                withAnimation(
+                    Animation
+                        .easeInOut(duration: animationDuration)
+                        .delay(animationDuration)) {
+                    isFilled = true
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Picker("Tuning",
+                           selection: viewStore.binding(
+                            get: \.tuning,
+                            send: Root.Action.changeTuning)
+                    ) {
+                        ForEach(Root.Tuning.allCases) { tuning in
+                            Text(tuning.rawValue)
+                        }
+                    }
+                }
+                ToolbarItem {
+                    Picker("Key", selection:
+                            viewStore.binding(
+                                get: \.rootNote,
+                                send: Root.Action.changeKey)
+                    ) {
+                        ForEach(Key.keysWithFlats, id: \.self) { key in
+                            Text(key.description)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+// MARK:- GuitarView
+struct GuitarView: View {
+    let notes = ["E","A","D","G","B","E"]
+    
+    var isFilled: Bool
+    let shadowRadius:CGFloat
+    var primaryColor: Color
+    var secondaryColor: Color
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+                ShapeView(bezier: GuitarShape.fretboard.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(gradient([.white, .black]))
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.fret.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(gradient([.white, .gray]))
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.nut.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(gradient([.white, .gray]))
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.rearTunersBase.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.rearTunersStem.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(secondaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.rearTunersKey.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+            }
+            
+            ZStack {
+                ShapeView(bezier: GuitarShape.guitarHeadstock.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(gradient([.accentColor, .gray]))
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.frontTunersBase.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.frontTunersBolt.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(secondaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+            }
+            
+            ZStack {
+                ShapeView(bezier: GuitarShape.guitarStrings.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(gradient([.white, .gray]))
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.stringTreeBase.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.stringTreeBolt.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(secondaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.frontTunersPeg.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+                
+                ShapeView(bezier: GuitarShape.tunerLines.path, pathBounds: GuitarShape.pathBounds)
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .shadow(radius: shadowRadius)
+            }
+        }
+    }
+}
+
+// MARK:- TunerButtonView
+
+struct TunersView: View {
+    var isFilled: Bool
+    var notes: [String]
+    var primaryColor: Color
+    
+    var body: some View {
+        VStack {
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[0]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: 42)
+            
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[1]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: -17)
+            
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[2]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: -77)
+            
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[3]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: -137)
+            
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[4]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: -196)
+            
+            Button(action: {}) {
+                Circle()
+                    .fill(primaryColor)
+                    .opacity(isFilled ? 1 : 0)
+                    .overlay(Text(notes[5]))
+            }
+            .padding(30)
+            .buttonStyle(PlainButtonStyle())
+            .position(x: 30, y: -256)
+        }
+    }
+}
+
+struct GuitarView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView(store: Root.defaultStore)
+    }
+}
+
+
+
+
+
+
